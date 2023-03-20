638ab2ac163be152fae8bc6fef6275e835419c95
Nik Von Huben <nik.vonhuben@silabs.com>
2023-03-20 23:14:02 +1100

    Update spdlog

--- components/spdlog/master/out/build/x64-Debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/silabs/github/BOB/components/spdlog/master/tests
-# Build directory: C:/silabs/github/BOB/components/spdlog/master/out/build/x64-Debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(spdlog-utests "C:/silabs/github/BOB/components/spdlog/master/out/build/x64-Debug/tests/spdlog-utests.exe")
-set_tests_properties(spdlog-utests PROPERTIES  _BACKTRACE_TRIPLES "C:/silabs/github/BOB/components/spdlog/master/tests/CMakeLists.txt;23;add_test;C:/silabs/github/BOB/components/spdlog/master/tests/CMakeLists.txt;0;")
